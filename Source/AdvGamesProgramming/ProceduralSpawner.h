aba0f47810fb77bb3b09e9ea31f333069d842de6
Vivian <vivianli7778@gmail.com>
2020-10-08 19:01:22 +1100

    Auto stash before merge of "Ass3/Vivian" and "Ass3/Procedural"

---
+++ Source/AdvGamesProgramming/ProceduralSpawner.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ADVGAMESPROGRAMMING_API ProceduralSpawner
+{
+public:
+	ProceduralSpawner();
+	~ProceduralSpawner();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay();
+
+
+
+};
