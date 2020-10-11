6f280bd637ed71eeadda4457a61464d2ee3bbb31
Vivian <vivianli7778@gmail.com>
2020-10-11 16:02:10 +1100

    Cleaning up stuff

--- Source/AdvGamesProgramming/CoverNode.h
+++ Source/AdvGamesProgramming/CoverNode.h
@@ -13,5 +13,16 @@ UCLASS()
 class ADVGAMESPROGRAMMING_API ACoverNode : public ANavigationNode
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this actor's properties
+	ACoverNode();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 };
